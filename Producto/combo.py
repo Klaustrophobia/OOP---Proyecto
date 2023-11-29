948952e730ff6aaeba0862fa7b9ba38510bc7364
Klaustrophobia <Lalosempai2@gmail.com>
2023-11-28 23:37:47 -0600

    ACT X

--- Producto/combo.py
+++ Producto/combo.py
@@ -3,6 +3,6 @@ class Combo(Producto):
     def __init__(self, codigo, marca, material, costo, existencia, tamano, nombre, productos):
         super().__init__(codigo, marca, material, costo, existencia, tamano)
         self.nombre = nombre
-        self.productos = productos
+        self.productos = []
     
     
